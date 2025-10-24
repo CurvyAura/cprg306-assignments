4d1a16fcaabb6f3cf527067fdaf6470ac1c57d62
Brett <CurvyAura@gmail.com>
2025-10-23 22:57:51 -0600

    week-7

---
+++ app/week-7/item.js
@@ -0,0 +1,9 @@
+export default function Item({name, quantity, category}) {
+    return (
+        <li className="border border-gray-500 p-3 mb-2 bg-gray-900 text-white">
+            <div className="text-lg">{name}</div>
+            <div className="text-sm">Quantity: {quantity}</div>
+            <div className="text-sm">Category: {category}</div>
+        </li>
+    )
+}
